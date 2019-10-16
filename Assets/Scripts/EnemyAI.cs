567aea9bd72c75619f1367260d52942ea672f918
José Manuel <josemac95@gmail.com>
2019-10-16 18:31:04 +0200

    168. Using NavMeshAgent for AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+	// Jugador
+	[SerializeField] Transform target = null;
+	// AI del enemigo
+	NavMeshAgent navMeshAgent;
+
+	void Start()
+	{
+		navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		// Va hacia el jugador
+		navMeshAgent.SetDestination(target.position);
+	}
+}
