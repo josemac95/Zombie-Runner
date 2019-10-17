c5661acc755c3ed11e92a6c6bbd014d09c66726f
José Manuel <josemac95@gmail.com>
2019-10-17 17:57:42 +0200

    174. Introducing to raycasting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+	// Cámara de la primera persona
+	[SerializeField] Camera FPCamera = null;
+	// Rango del rayo
+	[SerializeField] float range = 100f;
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shoot();
+		}
+	}
+
+	// Dispara
+	private void Shoot()
+	{
+		// Variable con la información del impacto
+		RaycastHit hit;
+		// Castea el rayo
+		bool impacted = Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+		// Si hay impacto
+		if (impacted)
+		{
+			print(hit.collider.name);
+		}
+	}
+}
