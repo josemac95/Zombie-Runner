8417674e8e04ac5115ea7a589808fa26727993dd
José Manuel <josemac95@gmail.com>
2019-10-19 11:15:13 +0200

    182. Use animation events

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+	// Objetivo al que ataca
+	[SerializeField] Transform target;
+	// Daño por impacto
+	[SerializeField] float damage = 40f;
+
+	void Start()
+	{
+
+	}
+
+	// Se llama en la animación de ataque
+	public void AttackHitEvent()
+	{
+		if (target != null)
+		{
+			print("PUM! " + damage + " damage");
+		}
+	}
+}
