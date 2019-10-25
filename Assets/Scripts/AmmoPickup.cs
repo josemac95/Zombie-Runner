2cecb7735ccbe079784f1879f77f0bf9a92808a5
José Manuel <josemac95@gmail.com>
2019-10-25 17:56:06 +0200

    200. Ammo pickup - Part 1

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+	// Manejo de la colisión
+	private void OnTriggerEnter(Collider other)
+	{
+		// Tag player
+		if (other.tag == "Player")
+		{
+			print("PICKUP");
+		}
+	}
+}
