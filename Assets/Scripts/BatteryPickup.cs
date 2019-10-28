5d69dfd49509b2dbbb3be1ffa213f2c9c7b20662
José Manuel <josemac95@gmail.com>
2019-10-28 11:41:02 +0100

    212. Creating a battery pickup

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+	// Cantidad de intensidad que restaura
+	[SerializeField] int intensityAmount = 1;
+	// Cantidad de ángulo que restaura
+	[SerializeField] int angleAmount = 70;
+
+	// Manejo de la colisión
+	private void OnTriggerEnter(Collider other)
+	{
+		// Tag player
+		if (other.tag == "Player")
+		{
+			// Restaura la luz
+			// Se podría poner FindObjectOfType<FlashLightSystem>()
+			other.GetComponentInChildren<FlashLightSystem>().RestoreLight(intensityAmount, angleAmount);
+			// Destruye el pickup
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/FlashLightSystem.cs
+++ Assets/Scripts/FlashLightSystem.cs
@@ -5,18 +5,24 @@ public class FlashLightSystem : MonoBehaviour
 {
 	// La intensidad de la luz decrece con el uso
 	[SerializeField] float intensityDecay = 0.1f;
+	// Intensidad máxima
+	[SerializeField] float maxIntensity = 2f;
 	// Intensidad mínima
 	[SerializeField] float minIntensity = 0f;
 	// El ángulo decrece con el uso
 	[SerializeField] float angleDecay = 1.5f;
 	// Ángulo mínimo
 	[SerializeField] float minAngle = 40f;
+	// Ángulo máximo
+	[SerializeField] float maxAngle = 70f;
 	// La luz
 	Light myLight;
 
 	void Start()
 	{
 		myLight = gameObject.GetComponent<Light>();
+		myLight.intensity = maxIntensity;
+		myLight.spotAngle = maxAngle;
 	}
 
 	void Update()
@@ -41,5 +47,12 @@ public class FlashLightSystem : MonoBehaviour
 		{
 			myLight.spotAngle -= angleDecay * Time.deltaTime;
 		}
+	}
+
+	// Reestablece la luz
+	public void RestoreLight(float intensityAmount, float angleAmount)
+	{
+		myLight.intensity = Mathf.Min(maxIntensity, myLight.intensity + intensityAmount);
+		myLight.spotAngle = Mathf.Min(maxAngle, myLight.spotAngle + angleAmount);
 	}
 }
